77c17ef10ca8785f24218d0a9aece1cc24cf81c7
Your name <tahrijouteyamer@gmail.com>
2023-03-22 09:32:38 -0700

    .

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,21 +7,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 #endif
 
-1- main.c
-#include <stdio.h>
-/**
-* main - causes an infinite loop
-* Return: 0
-*/
-int main(void)
-{
-int i;
-printf("Infinite loop incoming :(\n");
-i = 0;
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
-}
