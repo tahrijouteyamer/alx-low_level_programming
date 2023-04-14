9f0f096d803907b42570ef84e22d7acbd55ade9b
Your name <tahrijouteyamer@gmail.com>
2023-04-13 19:30:40 -0700

    .

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,114 +1,123 @@
-#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * is_number - checks if a string is a number
+ * @s: the string to check
  *
- * Return: 1 if digit, 0 otherwise
+ * Return: 1 if s is a number, 0 otherwise
  */
-int _isdigit(int c)
+
+int is_number(char *s)
 {
-	return (c >= '0' && c <= '9');
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (0);
+	}
+	return (1);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
+ * error - prints an error message and exits the program with code 98
  */
-int _strlen(char *s)
+
+void error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * str_len - calculates the length of a string
+ * @str: the string to calculate the length of
+ *
+ * Return: the length of str
+ */
+
+int str_len(char *str)
 {
 	int i = 0;
 
-	while (*s++)
+	while (str[i])
 		i++;
 	return (i);
 }
 
 /**
- * big_multiply - multiply two big number strings
- * @s1: the first big number string
- * @s2: the second big number string
- *
- * Return: the product big number string
+ * compute_mult - computes the product of two numbers represented as strings
+ * @len1: the length of the first number string
+ * @len2: the length of the second number string
+ * @s1: the first number string
+ * @s2: the second number string
+ * @result: an array to store the resulting product
  */
-char *big_multiply(char *s1, char *s2)
+
+void compute_mult(int len1, int len2, char *s1, char *s2, int *result)
 {
-	char *r;
-	int l1, l2, a, b, c, x;
+	int carry = 0;
+	int digit1 = 0;
+	int digit2 = 0;
+	int i, j;
 
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	r = malloc(a = x = l1 + l2);
-	if (!r)
-		printf("Error\n"), exit(98);
-	while (a--)
-		r[a] = 0;
+	for (i = 0; i < len1 + len2 + 1; i++)
+		result[i] = 0;
 
-	for (l1--; l1 >= 0; l1--)
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		if (!_isdigit(s1[l1]))
+		digit1 = s1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			free(r);
-			printf("Error\n"), exit(98);
+			digit2 = s2[j] - '0';
+			carry += result[i + j + 1] + (digit1 * digit2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
 		}
-		a = s1[l1] - '0';
-		c = 0;
-
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			if (!_isdigit(s2[l2]))
-			{
-				free(r);
-				printf("Error\n"), exit(98);
-			}
-			b = s2[l2] - '0';
-
-			c += r[l1 + l2 + 1] + (a * b);
-			r[l1 + l2 + 1] = c % 10;
-
-			c /= 10;
-		}
-		if (c)
-			r[l1 + l2 + 1] += c;
+		if (carry > 0)
+			result[i + j + 1] = carry;
 	}
-	return (r);
 }
 
-
 /**
- * main - multiply two big number strings
- * @argc: the number of arguments
- * @argv: the argument vector
+ * main - the entry point of the program
+ * @ac: the number of command-line arguments
+ * @av: an array of strings representing the command-line arguments
  *
- * Return: Always 0 on success.
+ * Return: 0 on success, 98 on failure
  */
-int main(int argc, char **argv)
+
+int main(int ac, char *av[])
 {
-	char *r;
-	int a, c, x;
+	int len1 = 0, len2 = 0, *result;
+	char *s1, *s2;
+	int checker = 0;
+	int i;
 
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-
-	x = _strlen(argv[1]) + _strlen(argv[2]);
-	r = big_multiply(argv[1], argv[2]);
-	c = 0;
-	a = 0;
-	while (c < x)
+	if (ac != 3)
+		error();
+	if (!(is_number(av[1]) && is_number(av[2])))
+		error();
+	s1 = av[1];
+	s2 = av[2];
+	len1 = str_len(s1);
+	len2 = str_len(s2);
+	result = malloc(sizeof(int) * (len1 + len2 + 1));
+	for (i = 0; i < len1 + len2 + 1; i++)
+		result[i] = 0;
+	compute_mult(len1, len2, s1, s2, result);
+	for (i = 0; i < len1 + len2; i++)
 	{
-		if (r[c])
-			a = 1;
-		if (a)
-			_putchar(r[c] + '0');
-		c++;
+		if (result[i])
+			checker = 1;
+		if (checker)
+			putchar(result[i] + 48);
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(r);
+	if (checker == 0)
+		putchar('0');
+	putchar('\n');
+	free(result);
 	return (0);
 }
