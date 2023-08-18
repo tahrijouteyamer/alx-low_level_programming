abc6b3b0c896b58362faeced86dfafd043565f93
Amer <tahrijouteyamer@hotmail.com>
2023-08-17 18:57:17 -0700

    .

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-largerst_palindrome = 0
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+largest_palindrome = 0
 
 for i in range(100, 1000):
-    for j in range(100, 1000):
+    for j in range(i, 1000):
         product = i * j
-        if str(product) == str(product)[::-1] and product > largest_palindrome:
+        if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
 
-with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
+with open("102-result", "w") as f:
+    f.write(str(largest_palindrome))
