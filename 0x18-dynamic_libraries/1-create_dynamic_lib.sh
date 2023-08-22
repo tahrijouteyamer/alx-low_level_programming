085e566a5b91a13d495e2364d32d7a9bbfa8312e
Amer <tahrijouteyamer@hotmail.com>
2023-08-21 18:46:19 -0700

    .

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
+gcc -c -fPIC *.c
+gcc -shared -o liball.so *.o
+nm -D liball.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
